3995811e7db210d267d79b7331748a6f32d04007
Koby Aharon <kobyahron@gmail.com>
2019-10-18 21:09:09 +0300

    WIP: Add sqs-connector

---
+++ sqs-connector/src/main/java/edu/ka/testingbed/cloudconfigmongo/sqsconnector/SqsConnectorApplication.java
@@ -0,0 +1,13 @@
+package edu.ka.testingbed.cloudconfigmongo.sqsconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsConnectorApplication.class, args);
+	}
+
+}
---
+++ sqs-connector/src/test/java/edu/ka/testingbed/cloudconfigmongo/sqsconnector/SqsConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ka.testingbed.cloudconfigmongo.sqsconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
