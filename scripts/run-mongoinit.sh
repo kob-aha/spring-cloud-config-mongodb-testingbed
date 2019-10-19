e326bc869e8a34098763a9c59e8841a2f3c01473
Koby Aharon <kobyahron@gmail.com>
2019-10-19 22:23:37 +0300

    Fix issues in run scripts after moving to scripts folder

--- scripts/run-mongoinit.sh
+++ scripts/run-mongoinit.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash 
 MONGO_IP=$( docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongo )
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_DIR="$SCRIPTS_DIR/.."
 DB_NAME="config"
 
 if [ -z "${MONGO_IP}" ]; then
@@ -8,4 +9,4 @@ if [ -z "${MONGO_IP}" ]; then
   exit 1
 fi
 
-docker run --net mongo -ti -v "${DIR}:/src" --rm --name mongo-client mongo mongo --host ${MONGO_IP} ${DB_NAME} /src/config-server/src/main/resources/init-config-database.js
+docker run --net mongo -ti -v "${ROOT_DIR}:/src" --rm --name mongo-client mongo mongo --host ${MONGO_IP} ${DB_NAME} /src/config-server/src/main/resources/init-config-database.js
