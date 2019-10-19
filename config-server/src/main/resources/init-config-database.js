70f74116d36ec4489ee4845a5c43f10c7db03674
Koby Aharon <kobyahron@gmail.com>
2019-10-19 22:42:59 +0300

    Replace AMQP binder with SQS

--- config-server/src/main/resources/init-config-database.js
+++ config-server/src/main/resources/init-config-database.js
@@ -5,6 +5,20 @@ db['mongo-config-testingbed'].insert({
     "app": {
       "message": "Hello from Spring cloud config MongoDB",
     },
+    "cloud": {
+      "aws": {
+        "credentials": {
+          "useDefaultAwsCredentialsChain": "true"
+        },
+        "stack": {
+          "auto": "false"
+        },
+        "region": {
+          "static": "us-east-2",
+          "auto": "false"
+        }
+      }
+    },
     "spring": {
       "rabbitmq": {
         "host": "localhost",
